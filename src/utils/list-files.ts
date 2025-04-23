ed1d5a075203f6680cfceafc7f0a2cc856f2f749
ZebraRoy <elcngchunyik@gmail.com>
2025-04-23 14:37:06 +0800

    add list files

--- src/utils/list-files.ts
+++ src/utils/list-files.ts
@@ -1,29 +1,66 @@
 import { glob } from "glob"
+import { simpleGit } from "simple-git"
+import binaryExtensions from "binary-extensions"
+import path from "path"
 
 export type ListFilesMode = "glob" | "changed" | "staged" | "last-commit"
+
+function filterUnique(file: string, index: number, self: string[]) {
+  return self.indexOf(file) === index
+}
+
+// filter out binary files using https://github.com/sindresorhus/binary-extensions
+function filterBinaryFile(file: string) {
+  const extension = path.extname(file).slice(1)
+  return !binaryExtensions.includes(extension)
+}
+
+function filterEmptyFile(file: string) {
+  return file.trim() !== ""
+}
+
+function normalizeFilePath(files: string[]) {
+  return files.filter(filterBinaryFile).filter(filterUnique).filter(filterEmptyFile)
+}
 
 const listMethodMap: Record<ListFilesMode, (root: string, globPattern?: string) => Promise<string[]>> = {
   "glob": async (root: string, globPattern?: string) => {
     if (!globPattern) {
       throw new Error("globPattern is required")
     }
+    console.log("globPattern", globPattern, root)
     const globResult = await glob(
       globPattern,
       {
         cwd: root,
-        absolute: true,
       },
     )
-    return globResult
+    return normalizeFilePath(globResult)
   },
   "changed": async (root: string) => {
-    return []
+    const git = simpleGit({
+      baseDir: root,
+    })
+    const status = await git.status()
+    const changedFiles = status.modified
+    const addedFiles = status.created
+    const notAddedFiles = status.not_added
+    return normalizeFilePath([...changedFiles, ...addedFiles, ...notAddedFiles])
   },
   "staged": async (root: string) => {
-    return []
+    const git = simpleGit({
+      baseDir: root,
+    })
+    const status = await git.status()
+    const stagedFiles = status.staged
+    return normalizeFilePath(stagedFiles)
   },
   "last-commit": async (root: string) => {
-    return []
+    const git = simpleGit({
+      baseDir: root,
+    })
+    const lastCommitFileList = await git.show(["--name-only", "--pretty=format:"])
+    return normalizeFilePath(lastCommitFileList.split("\n"))
   },
 }
 
